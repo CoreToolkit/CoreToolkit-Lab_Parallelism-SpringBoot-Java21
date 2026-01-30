856d23b5bdf80e4cca62a8fb3ebec79689f838f3
DaniAleRoB <daniel.rodriguez-b@mail.escuelaing.edu.co>
2026-01-29 19:32:41 -0500

    docs: add Swagger documentation and integration tests

--- src/main/java/edu/eci/arsw/parallelism/api/PiCalculationException.java
+++ src/main/java/edu/eci/arsw/parallelism/api/PiCalculationException.java
@@ -7,7 +7,6 @@ package edu.eci.arsw.parallelism.api;
  */
 public class PiCalculationException extends RuntimeException {
 
-    // Error message constants
     public static final String INVALID_START_NEGATIVE = "Parameter 'start' must be greater than or equal to 0";
     public static final String INVALID_COUNT_ZERO_OR_NEGATIVE = "Parameter 'count' must be at least 1";
     public static final String INVALID_PARAMETER_TYPE = "Invalid parameter type. Expected a number";
